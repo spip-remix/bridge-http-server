94506585033461ae2844ee9be2087f81e8006c91
JamesRezo <james@rezo.net>
2024-02-08 16:19:30 +0100

    feat(Converter): setup

---
+++ src/Converter.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Spip\Component\Http;
+
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7Server\ServerRequestCreator;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @codeCoverageIgnore
+ */
+class Converter
+{
+    private PsrHttpFactory $psrHttpFactory;
+
+    public function __construct(
+        private Psr17Factory $psr17Factory,
+        private HttpFoundationFactory $httpFoundationFactory,
+    ) {
+        $this->psrHttpFactory = new PsrHttpFactory(
+            $psr17Factory,
+            $psr17Factory,
+            $psr17Factory,
+            $psr17Factory,
+        );
+    }
+
+    public function fromSymfonyRequest(Request $request): ServerRequestInterface
+    {
+        return $this->psrHttpFactory->createRequest($request);
+    }
+
+    public function fromSymfonyResponse(Response $response): ResponseInterface
+    {
+        return $this->psrHttpFactory->createResponse($response);
+    }
+
+    public function fromPsrServerRequest(ServerRequestInterface $request): Request
+    {
+        return $this->httpFoundationFactory->createRequest($request);
+    }
+    
+    public function fromPsrResponse(ResponseInterface $response): Response
+    {
+        return $this->httpFoundationFactory->createResponse($response);
+    }
+
+    public static function fromGlobals(): ServerRequestInterface
+    {
+        $factory = new Psr17Factory;
+        $creator = new ServerRequestCreator(
+            $factory, // ServerRequestFactory
+            $factory, // UriFactory
+            $factory, // UploadedFileFactory
+            $factory  // StreamFactory
+        );
+
+        return $creator->fromGlobals();
+    }
+}
---
+++ tests/ConverterTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Spip\Component\Http\Test;
+
+use Nyholm\Psr7\Factory\Psr17Factory;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ServerRequestInterface;
+use Spip\Component\Http\Converter;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Component\HttpFoundation\Request;
+
+#[CoversClass(Converter::class)]
+class ConverterTest extends TestCase
+{
+    private Converter $converter;
+
+    protected function setUp(): void
+    {
+        $this->converter = new Converter(
+            new Psr17Factory,
+            new HttpFoundationFactory()
+        );
+    }
+
+    public function testFromSymfonyRequest(): void
+    {
+        // Given
+        $request = Request::create('/test?param=1');
+        $expectedValue = $request->get('param');
+
+        // When
+        $actual = $this->converter->fromSymfonyRequest($request);
+
+        // Then
+        if (!isset($actual->getQueryParams()['param'])) {
+            $this->fail('param not converted');
+        }
+        $this->assertInstanceOf(ServerRequestInterface::class, $actual);
+        $this->assertEquals($expectedValue, $actual->getQueryParams()['param']);
+    }
+}
